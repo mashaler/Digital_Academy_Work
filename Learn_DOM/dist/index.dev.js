9b1c66278c9ebd3d867f385f1f514eeb8bb910fd
mashaler <mashalerolva@gmail.com>
2023-04-12 19:13:51 +0200

    Updated

---
+++ Learn_DOM/dist/index.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// EXERCISE 1
+function style() {
+  document.getElementById("text").style.color = "red";
+  document.getElementById("text").style.fontWeight = "bold";
+  document.getElementById("text").style.fontSize = "large";
+} // EXERCISE 2  
+
+
+function getFormValue() {}
+//# sourceMappingURL=index.dev.js.map
---
+++ Learn_DOM/index.js
@@ -0,0 +1,12 @@
+// EXERCISE 1
+function style(){
+
+    document.getElementById("text").style.color="red"
+    document.getElementById("text").style.fontWeight="bold"
+    document.getElementById("text").style.fontSize="large"
+  }
+
+  // EXERCISE 2  
+function getFormValue(){
+    
+}
