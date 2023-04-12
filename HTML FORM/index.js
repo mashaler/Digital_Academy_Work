8d4a79508cb9ac09b53b27ed686e3b92b904f3d8
mashaler <mashalerolva@gmail.com>
2023-04-12 16:32:25 +0200

    updated

--- HTML FORM/index.js
+++ HTML FORM/index.js
@@ -2,12 +2,11 @@ function check(){
     let fname = document.getElementById("fname").value;
     let lname = document.getElementById("lname").value;
     let email = document.getElementById("email").value;
-    
+
     console.log(`
     first name: ${fname}\n
     last name: ${lname}\n
     email:${email}\n
     `);
-
 }
 
